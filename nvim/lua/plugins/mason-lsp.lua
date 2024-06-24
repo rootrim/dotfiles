e23e56e7bc50c6019178fa195cdc60692abf2616
rootrim <omuaz01@gmail.com>
2024-06-24 23:19:53 +0300

    The big neovim update

---
+++ nvim/lua/plugins/mason-lsp.lua
@@ -0,0 +1,39 @@
+return {
+   {
+      "williamboman/mason.nvim",
+      lazy = false,
+      config = function()
+         require("mason").setup()
+      end,
+   },
+   {
+      "williamboman/mason-lspconfig.nvim",
+      lazy = false,
+      config = function()
+         require("mason-lspconfig").setup({
+            ensure_installed = { "lua_ls", "pyright" },
+         })
+      end,
+   },
+   {
+      "neovim/nvim-lspconfig",
+      lazy = false,
+      config = function()
+         local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+         local lspconfig = require("lspconfig")
+         lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+         })
+         lspconfig.pyright.setup({
+            capabilities = capabilities,
+         })
+
+         vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+         vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+         vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+         vim.keymap.set("n", "ca", vim.lsp.buf.code_action, {})
+         vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      end,
+   },
+}
---
+++ nvim/lua/vimptions.lua
@@ -0,0 +1,6 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=3")
+vim.cmd("set softtabstop=3")
+vim.cmd("set shiftwidth=3")
+vim.g.mapleader = " "
+vim.opt.number = true
