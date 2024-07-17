4338d9bbc72c9543ba606750f7e2b2ad64ecf585
rootrim <omuaz01@gmail.com>
2024-07-17 13:26:59 +0300

    Added tab shortcuts to nvim

--- nvim/lua/vimptions.lua
+++ nvim/lua/vimptions.lua
@@ -4,3 +4,8 @@ vim.cmd("set softtabstop=3")
 vim.cmd("set shiftwidth=3")
 vim.g.mapleader = " "
 vim.opt.number = true
+vim.keymap.set("n", "<leader>tt", ":tabnew<CR>", {})
+vim.keymap.set("n", "<leader>tn", ":tabnext<CR>", {})
+vim.keymap.set("n", "<leader>tp", ":tabprev<CR>", {})
+vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", {})
+vim.keymap.set("n", "<leader>to", ":tabonly<CR>", {})
