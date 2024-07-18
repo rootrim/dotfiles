ec7771d381e1e136891be5f6c23d6097f3962c6a
rootrim <omuaz01@gmail.com>
2024-07-18 12:47:56 +0300

    Removed waybar and some changes

--- nvim/lua/plugins/mason-lsp.lua
+++ nvim/lua/plugins/mason-lsp.lua
@@ -1,62 +1,74 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pyright", "bashls", "html", "cssls", "taplo", "markdown_oxide" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+   {
+      "williamboman/mason.nvim",
+      lazy = false,
+      config = function()
+         require("mason").setup()
+      end,
+   },
+   {
+      "williamboman/mason-lspconfig.nvim",
+      lazy = false,
+      config = function()
+         require("mason-lspconfig").setup({
+            ensure_installed = {
+               "lua_ls",
+               "pyright",
+               "bashls",
+               "html",
+               "cssls",
+               "taplo",
+               "markdown_oxide",
+               "jsonls",
+            },
+         })
+      end,
+   },
+   {
+      "neovim/nvim-lspconfig",
+      lazy = false,
+      config = function()
+         local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.bashls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.taplo.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.markdown_oxide.setup({
-				capabilities = capabilities,
-			})
+         local lspconfig = require("lspconfig")
+         lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+         })
+         lspconfig.pyright.setup({
+            capabilities = capabilities,
+         })
+         lspconfig.bashls.setup({
+            capabilities = capabilities,
+         })
+         lspconfig.html.setup({
+            capabilities = capabilities,
+         })
+         lspconfig.cssls.setup({
+            capabilities = capabilities,
+         })
+         lspconfig.taplo.setup({
+            capabilities = capabilities,
+         })
+         lspconfig.markdown_oxide.setup({
+            capabilities = capabilities,
+         })
+         lspconfig.jsonls.setup({
+            capabilities = capabilities,
+         })
 
-			lspconfig.hyprls.setup({
-				cmd = { "hyprls" },
-				root_dir = function(fname)
-					return vim.fn.getcwd() -- veya uygun bir root_dir fonksiyonu
-				end,
-				capabilities = capabilities,
-			})
+         lspconfig.hyprls.setup({
+            cmd = { "hyprls" },
+            root_dir = function(fname)
+               return vim.fn.getcwd() -- veya uygun bir root_dir fonksiyonu
+            end,
+            capabilities = capabilities,
+         })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+         vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+         vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+         vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+         vim.keymap.set("n", "ca", vim.lsp.buf.code_action, {})
+         vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      end,
+   },
 }
