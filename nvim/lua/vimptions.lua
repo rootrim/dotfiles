53b4162cdd9bc4c2d937b35317571b073e90f219
rootrim <omuaz01@gmail.com>
2024-07-31 01:00:05 +0300

    Tmux and kanata update

--- nvim/lua/vimptions.lua
+++ nvim/lua/vimptions.lua
@@ -3,9 +3,12 @@ vim.cmd("set tabstop=3")
 vim.cmd("set softtabstop=3")
 vim.cmd("set shiftwidth=3")
 vim.g.mapleader = " "
+vim.g.backround = "light"
 vim.opt.number = true
-vim.keymap.set("n", "<leader>tt", ":tabnew<CR>", {})
-vim.keymap.set("n", "<leader>tn", ":tabnext<CR>", {})
-vim.keymap.set("n", "<leader>tp", ":tabprev<CR>", {})
-vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", {})
-vim.keymap.set("n", "<leader>to", ":tabonly<CR>", {})
+
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':noh<CR>')
