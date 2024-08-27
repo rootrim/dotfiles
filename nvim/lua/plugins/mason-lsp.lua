7562070dfb4d0e7f7606b08f185db73bbe35dc53
rootrim <omuaz01@gmail.com>
2024-08-27 19:36:45 +0300

    startup.conf changes, mason-lsp.lua changes, added custom firefox module on waybar and added 2 alias to zsh

--- nvim/lua/plugins/mason-lsp.lua
+++ nvim/lua/plugins/mason-lsp.lua
@@ -2,14 +2,6 @@ return {
    {
       "williamboman/mason.nvim",
       lazy = false,
-      opts = {
-         ensure_installed = {
-            "mypy",
-            "ruff",
-            "pyright",
-            "stylua"
-         }
-      },
       config = function()
          require("mason").setup()
       end,
@@ -28,44 +20,34 @@ return {
          local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
          local lspconfig = require("lspconfig")
+         lspconfig.cssls.setup({
+            capabilities = capabilities,
+         })
+         lspconfig.tsserver.setup({
+            capabilities = capabilities,
+         })
+         lspconfig.html.setup({
+            capabilities = capabilities,
+         })
          lspconfig.lua_ls.setup({
             capabilities = capabilities,
          })
          lspconfig.pyright.setup({
             capabilities = capabilities,
          })
-         lspconfig.bashls.setup({
-            capabilities = capabilities,
-         })
-         lspconfig.html.setup({
-            capabilities = capabilities,
-         })
-         lspconfig.cssls.setup({
-            capabilities = capabilities,
-         })
-         lspconfig.taplo.setup({
-            capabilities = capabilities,
-         })
-         lspconfig.markdown_oxide.setup({
-            capabilities = capabilities,
-         })
-         lspconfig.jsonls.setup({
-            capabilities = capabilities,
-         })
-
          lspconfig.hyprls.setup({
-            cmd = { "hyprls" },
-            root_dir = function()
-               return vim.fn.getcwd() -- veya uygun bir root_dir fonksiyonu
-            end,
+            capabilities = capabilities,
+         })
+         lspconfig.bashls.setup({
             capabilities = capabilities,
          })
 
          vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-         vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-         vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-         vim.keymap.set("n", "ca", vim.lsp.buf.code_action, {})
-         vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+         vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+         vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
+         vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
       end,
    },
 }
