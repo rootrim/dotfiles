d8ea6101a8681aa4850248cb71fdfc27f7f25942
rootrim <omuaz01@gmail.com>
2024-10-26 22:46:20 +0300

    Not a Big update

--- nvim/lua/plugins/image.lua
+++ nvim/lua/plugins/image.lua
@@ -2,6 +2,14 @@ return {
   "3rd/image.nvim",
   dependencies = { "luarocks.nvim" },
   config = function()
-    -- ...
+    require("image").setup {
+      integrations = {
+        markdown = {
+          resolve_image_path = function(document_path, image_path, fallback)
+            return fallback(document_path, image_path)
+          end,
+        },
+      },
+    }
   end,
 }
