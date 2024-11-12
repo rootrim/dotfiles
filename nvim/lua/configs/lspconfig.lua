66a9dad3e6f74f2827d8363c121ee396e8fcfda7
gitrim <omuaz01@gmail.com>
2024-11-12 13:46:59 +0300

    LSP update

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -8,21 +8,51 @@ local capabilities = configs.capabilities
 local lspconfig = require "lspconfig"
 local servers = {
   "html",
+  "gopls",
   "cssls",
   "clangd",
-  -- "basedpyright",
-  "pyright",
+  "pylsp",
   "ruff_lsp",
+  "rust_analyzer",
+  "hls",
   "hyprls",
   "bashls",
   "markdown_oxide",
   "arduino_language_server",
+  "gleam",
 }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_init = on_init,
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+  if lsp ~= "pylsp" then
+    lspconfig[lsp].setup {
+      on_init = on_init,
+      on_attach = on_attach,
+      capabilities = capabilities,
+    }
+  elseif lsp == "pylsp" then
+    lspconfig.pylsp.setup {
+      on_init = on_init,
+      on_attach = on_attach,
+      capabilities = capabilities,
+      settings = {
+        pylsp = {
+          plugins = {
+            pydocstyle = { enabled = false }, -- Disable pydocstyle (docstring checks)
+            jedi_completion = {
+              enabled = true,
+              include_params = true,
+              fuzzy = true,
+              cache_for = { "numpy", "pandas", "matplotlib", "pytorch", "cv2" },
+            },
+            pyflakes = { enabled = true },
+            pycodestyle = { enabled = true, maxLineLength = 80, ignore = { "E501" } },
+            autopep8 = { enabled = true },
+            flake8 = { enabled = false },
+            yapf = { enabled = true },
+          },
+          jedi = { auto_import_modules = { "numpy", "matplotlib" } },
+        },
+      },
+    }
+  end
 end
