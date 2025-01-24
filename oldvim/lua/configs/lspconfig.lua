1b819d34440d470ff57f77b4063e0509264c4ab0
gitrim <omuaz01@gmail.com>
2025-01-24 20:09:18 +0300

    A lot of things which I can't remember
    Ok?

--- oldvim/lua/configs/lspconfig.lua
+++ oldvim/lua/configs/lspconfig.lua
@@ -4,6 +4,7 @@ local configs = require "nvchad.configs.lspconfig"
 local on_attach = configs.on_attach
 local on_init = configs.on_init
 local capabilities = configs.capabilities
+local util = require "lspconfig.util"
 
 local lspconfig = require "lspconfig"
 local servers = {
@@ -24,7 +25,7 @@ local servers = {
 }
 
 for _, lsp in ipairs(servers) do
-  if lsp ~= "pylsp" then
+  if lsp ~= "pylsp" or "hls" then
     lspconfig[lsp].setup {
       on_init = on_init,
       on_attach = on_attach,
@@ -46,12 +47,29 @@ for _, lsp in ipairs(servers) do
               cache_for = { "numpy", "pandas", "matplotlib", "pytorch", "cv2" },
             },
             pyflakes = { enabled = true },
-            pycodestyle = { enabled = true, maxLineLength = 80, ignore = { "E501" } },
+            pycodestyle = { enabled = true, maxLineLength = 80, ignore = { "E501", "E203" } },
             autopep8 = { enabled = true },
             flake8 = { enabled = false },
             yapf = { enabled = true },
           },
           jedi = { auto_import_modules = { "numpy", "matplotlib" } },
+        },
+      },
+    }
+  elseif lsp == "hls" then
+    lspconfig.hls.setup {
+      on_init = on_init,
+      on_attach = on_attach,
+      capabilities = capabilities,
+      cmd = { "haskell-language-server-wrapper", "--lsp" },
+      filetypes = { "haskell", "lhaskell" },
+      root_dir = util.root_pattern("hie.yaml", "stack.yaml", "cabal.project", "*.cabal", "package.yaml"),
+      single_file_support = true,
+      settings = {
+        haskell = {
+          hlintOn = true,
+          formattingProvider = "ormolu",
+          cabalFormattingProvider = "cabalfmt",
         },
       },
     }
